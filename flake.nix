4977e62dd5574bb88b9ad679f090aee9c5b88ed6
Shetty Yttehs <shetty.yttehs@outlook.com>
2026-03-04 23:16:47 +0800

    build(flake): add esp-dev as a dependency and use it in the dev shell

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
+    esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
   };
 
-  outputs = { self, nixpkgs, flake-utils, }:
+  outputs = { self, nixpkgs, flake-utils, esp-dev }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -26,6 +27,7 @@
         ];
       in {
         devShells.default = pkgs.mkShell {
+          inputsFrom = [ esp-dev.devShells.${system}.esp-idf-full ];
           packages = with pkgs; [ uv patchelf ];
           LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath runtimeLibs}:${
               pkgs.lib.concatStringsSep ":" openglDriverLibs
